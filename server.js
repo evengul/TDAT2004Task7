9a99594cb57405b817fd29c1a5574ef9cdb202ae
evengu <evengu@stud.ntnu.no>
2020-03-25 15:57:58 +0100

    Comments.

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 // Simple HTTP server responds with a simple WebSocket client test
 const httpServer = net.createServer(connection => {
     connection.on('data', () => {
+        //Changed the text html to reading a file
         let content = fs.readFileSync("./index.html");
         connection.write('HTTP/1.1 200 OK\r\nContent-Length: ' + content.length + '\r\n\r\n' + content);
     });
@@ -17,6 +18,7 @@ httpServer.listen(3000, () => {
     console.log('HTTP server listening on port 3000');
 });
 
+//Container for all connected clients that we will send data to
 let clients = [];
 
 // Incomplete WebSocket server
@@ -24,7 +26,9 @@ const wsServer = net.createServer(connection => {
     console.log('Client connected');
 
     connection.on('data', data => {
+        //If this is a standard data package
         if (!(data.toString().includes("HTTP/1.1"))){
+            //Parse the data
             let bytes = data;
             let length = bytes[1] & 127;
             let maskStart = 2;
@@ -36,38 +40,45 @@ const wsServer = net.createServer(connection => {
                 let byte = bytes[i] ^ bytes[maskStart + ((i - dataStart)) % 4];
                 parsedData += String.fromCharCode(byte);
             }
-            connection.write(frameData(parsedData));
 
+            //For every client
             for (let i = 0; i < clients.length; i++){
+                //If the client still exists, and is not this same client we are sending from
                 if (!clients[i].destroyed && clients[i] !== connection){
-                    console.log("writing");
-                    console.log(parsedData);
+                    //Write the data to all other clients than "yourself"
                     clients[i].write(frameData(parsedData));
                 }
+                //If this is our client
                 else if (clients[i] === connection){
+                    //Give a message that we have sent data to other clients
                     clients[i].write(frameData("Data sent to all other clients!"));
                 }
             }
         }
+        //If this is the first connection of this client to this websocket
         else{
-
+            //If the connection does not exist in the clients array, push it
             if (clients.indexOf(connection) === -1){
                 console.log("pushing");
                 clients.push(connection);
             }
 
+            //Retrieve the key from the http request. Remember to remove \r on the end of the line
             let clientKey = data.toString()
                 .split("\n")
                 .filter((line) => line.includes("Sec-WebSocket-Key"))[0].split(": ")[1].slice(0, -1);
 
+            //Create the server key we send back
             let serverKey = Base64.stringify(CryptoJS.SHA1(clientKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"));
 
+            //Create the http response
             let returnValue = "HTTP/1.1 101 Switching Protocols\r\n" +
                 "Upgrade: websocket\r\n" +
                 "Connection: Upgrade\r\n" +
                 "Sec-WebSocket-Accept: " + serverKey.trim() + "\r\n" +
                 "\r\n";
 
+            //Write the http response
             connection.write(returnValue);
         }
 
@@ -75,6 +86,7 @@ const wsServer = net.createServer(connection => {
 
     connection.on('end', () => {
         console.log('Client disconnected');
+        //Remove the client from the list of clients we send data to
         clients = clients.filter(e => e !== connection);
         connection.end();
         connection.destroy();
@@ -88,19 +100,20 @@ wsServer.listen(3001, () => {
     console.log('WebSocket server listening on port 3001');
 });
 
+//For framing data for sending through the websocket
 function frameData(str){
-    let type = 0x81;
-    let length = str.length;
-    if (length > 127){
+    let type = 0x81;                                //We send text messages
+    let length = str.length;                        //Of a certain length
+    if (length > 127){                              //We can only send 127 bytes at a time.
         throw new Error("Cannot process more than 127 bytes of sending. Make a buffer");
     }
-    let maskedAndLength = 0b00000000 | length;
-    let data = Buffer.from(str);
+    let maskedAndLength = 0b00000000 | length;      //We do not mask our data from the server. Leftmost bit would be 1 if we did.
+    let data = Buffer.from(str);                    //Get the bytes from the string we are sending
 
-    let frame = [type];
-    frame.push(maskedAndLength);
+    let frame = [type];                             //Add the type to our framed data
+    frame.push(maskedAndLength);                    //Add the mask bit and length bits to our framed data
     for (let i = 0; i < data.length; i++){
-        frame.push(data[i]);
+        frame.push(data[i]);                        //Add the data to our framed data
     }
-    return Buffer.from(frame);
+    return Buffer.from(frame);                      //Return a byte array that we can send to our clients through the socket.
 }
